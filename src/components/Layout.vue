a8591b9b124e43cb958f47618215e1ff17f52350
laohu <laohubbb@gmail.com>
2022-11-02 15:31:31 +0800

    大致完成layout布局

---
+++ src/components/Layout.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="layout">
+<!--    <sidebar></sidebar>-->
+    <div class="sidebar"></div>
+    <div class="container">
+      <div class="banner"></div>
+      <div class="content">hello</div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+import Sidebar from "@/components/Sidebar";
+export default {
+  name: 'Layout',
+  components:{
+    Sidebar
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.layout{
+  height: 100%;
+  display: flex;
+}
+.sidebar{
+  background-color:#2c3138;
+  height: 100%;
+  width: 20%;
+}
+.container{
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  .banner{
+    background-color:#2c3138;
+    height: 60px;
+    width: 100%;
+  }
+  .content{
+    border: 1px solid red;
+    height: 100%;
+    width: 100%;
+  }
+}
+
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  props: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.container{
+  background-color:#2c3138;
+  width: 160px;
+  height: 100%;
+}
+</style>
